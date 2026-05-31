f3f7a381cc202b18c8c82d26d5f1829babf89f8a
Morgan123321 <morgan.feeney1022@gmail.com>
2026-05-31 11:01:15 -0400

    java3

--- index.js
+++ index.js
@@ -4,13 +4,21 @@
 
 function contact(event) { 
     event.preventDefault();
-    emailjs
-    .sendForm(
-        'service_unxthal',
-        'template_6oomkop',
-        event.target,
-        'pP19id_5iI0771PQA'
-    ).then(() => {
-        console.log('this worked1')
-    })
+    // emailjs
+    // .sendForm(
+    //     'service_unxthal',
+    //     'template_6oomkop',
+    //     event.target,
+    //     'pP19id_5iI0771PQA'
+    // ).then(() => {
+    //             console.log('this worked1')
+    // })
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible";
+    setTimeout (() => {
+       loading.classList.remove("modal__overlay--visible");
+       success.classList += " modal__overlay--visible";
+       console.log('it worked 1')
+    },1000);
 }
